43a573d5c335b33bb78133d814f35ed284fb2005
Santte91 <santte.91@gmail.com>
2026-02-12 20:43:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando: Elías Contigo ✨');
+const VERIFY_TOKEN = "eliascontigo";
+
+app.get("/whatsapp", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK_VERIFICADO");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
