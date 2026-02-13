c24329ce69a5536f65188838296fe6a2b8285be8
Santte91 <santte.91@gmail.com>
2026-02-12 20:59:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const VERIFY_TOKEN = "eliascontigo";
+// Token que pusiste en Meta
+const VERIFY_TOKEN = 'eliascontigo';
 
-app.get("/whatsapp", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
+app.use(express.json());
 
-  if (mode && token) {
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      console.log("WEBHOOK_VERIFICADO");
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando: Elías Contigo 🌟');
+});
+
+app.get('/whatsapp', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK_VERIFICADO');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
